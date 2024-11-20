e9950f8c843c5fece5bacd3dac504082207161d0
Javier Chung <34190746+Javiebear@users.noreply.github.com>
2024-11-20 02:55:52 -0500

    Update objectTracker.py

--- objectTracker.py
+++ objectTracker.py
@@ -6,7 +6,7 @@ classes = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 # Loading video
